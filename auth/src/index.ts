583bed28678a28634546dfcd0eb5db94f298cd84
Dang Vu Nhat Quang <ghitacluber@gmail.com>
2021-01-25 15:13:26 +0700

    test deploy

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -21,7 +21,7 @@ const bootstrap = async () => {
     console.log(err);
   }
   app.listen(4000, () => {
-    console.log('Listening on port 4000 ...');
+    console.log('Listening on port 4000 ......');
   });
 };
 
